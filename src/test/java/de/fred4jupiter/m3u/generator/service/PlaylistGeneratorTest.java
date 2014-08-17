d3c4ca2bedf0ace93166439983c41586c3d87a2b
fred4jupiter <hamsterhase@gmx.de>
2014-08-17 14:39:35 +0200

    bugfix tests

--- src/test/java/de/fred4jupiter/m3u/generator/service/PlaylistGeneratorTest.java
+++ src/test/java/de/fred4jupiter/m3u/generator/service/PlaylistGeneratorTest.java
@@ -46,8 +46,8 @@ public class PlaylistGeneratorTest extends AbstractShellIntegrationTest {
         assertThat(commandResult.isSuccess(), equalTo(true));
 
         String playlistName = Constants.DEFAULT_PLAYLIST_NAME + PlaylistFileType.M3U.getFileExtension();
-        checkPlaylistContains(playlistName, GENESIS_ANOTHER_RECORD_FILE_NAME, GENESIS_KEEP_IT_DARKER_FILE_NAME);
-        checkPlaylistContains(playlistName, NIRVANA_COME_AS_YOU_ARE_FILE_NAME, NIRVANA_ON_A_PLAIN_FILE_NAME);
+        checkPlaylist(playlistName, 4, GENESIS_ANOTHER_RECORD_FILE_NAME, GENESIS_KEEP_IT_DARKER_FILE_NAME);
+        checkPlaylist(playlistName, 4, NIRVANA_COME_AS_YOU_ARE_FILE_NAME, NIRVANA_ON_A_PLAIN_FILE_NAME);
     }
 
     @Test
@@ -57,8 +57,8 @@ public class PlaylistGeneratorTest extends AbstractShellIntegrationTest {
         assertThat(commandResult.isSuccess(), equalTo(true));
 
         String playlistName = "MyList" + PlaylistFileType.M3U.getFileExtension();
-        checkPlaylistContains(playlistName, GENESIS_ANOTHER_RECORD_FILE_NAME, GENESIS_KEEP_IT_DARKER_FILE_NAME);
-        checkPlaylistContains(playlistName, NIRVANA_COME_AS_YOU_ARE_FILE_NAME, NIRVANA_ON_A_PLAIN_FILE_NAME);
+        checkPlaylist(playlistName, 4, GENESIS_ANOTHER_RECORD_FILE_NAME, GENESIS_KEEP_IT_DARKER_FILE_NAME);
+        checkPlaylist(playlistName, 4, NIRVANA_COME_AS_YOU_ARE_FILE_NAME, NIRVANA_ON_A_PLAIN_FILE_NAME);
     }
 
     @Test
@@ -68,8 +68,8 @@ public class PlaylistGeneratorTest extends AbstractShellIntegrationTest {
         assertThat(commandResult.isSuccess(), equalTo(true));
 
         String playlistName = Constants.DEFAULT_PLAYLIST_NAME + PlaylistFileType.M3U.getFileExtension();
-        checkPlaylistContains(playlistName, GENESIS_KEEP_IT_DARKER_FILE_NAME, GENESIS_ANOTHER_RECORD_FILE_NAME);
-        checkPlaylistContains(playlistName, NIRVANA_ON_A_PLAIN_FILE_NAME, NIRVANA_COME_AS_YOU_ARE_FILE_NAME);
+        checkPlaylist(playlistName, 4, GENESIS_KEEP_IT_DARKER_FILE_NAME, GENESIS_ANOTHER_RECORD_FILE_NAME);
+        checkPlaylist(playlistName, 4, NIRVANA_ON_A_PLAIN_FILE_NAME, NIRVANA_COME_AS_YOU_ARE_FILE_NAME);
     }
 
     @Test
@@ -120,38 +120,43 @@ public class PlaylistGeneratorTest extends AbstractShellIntegrationTest {
         assertThat(commandResult.isSuccess(), equalTo(true));
 
         String playlistName = Constants.DEFAULT_PLAYLIST_NAME + PlaylistFileType.M3U.getFileExtension();
-        List<String> files = Arrays.asList(GENESIS_ANOTHER_RECORD_FILE_NAME, GENESIS_KEEP_IT_DARKER_FILE_NAME, NIRVANA_COME_AS_YOU_ARE_FILE_NAME, NIRVANA_ON_A_PLAIN_FILE_NAME);
-        checkPlaylistInTarget(playlistName, targetDir, files);
+        checkPlaylistContains(playlistName, targetDir, Arrays.asList(GENESIS_ANOTHER_RECORD_FILE_NAME, GENESIS_KEEP_IT_DARKER_FILE_NAME), 4);
+        checkPlaylistContains(playlistName, targetDir, Arrays.asList(NIRVANA_COME_AS_YOU_ARE_FILE_NAME, NIRVANA_ON_A_PLAIN_FILE_NAME), 4);
     }
 
     private void checkPlaylist(String playlistName, String... fileNames) throws IOException {
-        checkPlaylistInTarget(playlistName, BASE_DIR, Arrays.asList(fileNames));
+        checkPlaylistContains(playlistName, BASE_DIR, Arrays.asList(fileNames), fileNames.length);
     }
 
-    private void checkPlaylistInTarget(String playlistName, String targetDir, List<String> fileNames) throws IOException {
-        String baseFolder = targetDir != null ? targetDir : BASE_DIR;
-        File playlistFile = new File(baseFolder + File.separator + playlistName);
-        assertThat("playlist does not exists: " + playlistFile, playlistFile.getAbsoluteFile().exists(), equalTo(true));
-
-        List<String> fileContent = IOUtils.readLines(new FileInputStream(playlistFile));
-        assertNotNull(fileContent);
-        assertThat(fileContent.size(), equalTo(fileNames.size()));
-
-        for (int i = 0; i < fileContent.size(); i++) {
-            assertThat(fileContent.get(i), containsString(getPathFor(fileNames.get(i))));
-        }
+    private void checkPlaylist(String playlistName, int numberOfEntries, String... fileNames) throws IOException {
+        checkPlaylistContains(playlistName, BASE_DIR, Arrays.asList(fileNames), numberOfEntries);
     }
 
-    private void checkPlaylistContains(String playlistName, String... fileNames) throws IOException {
-        File playlistFile = new File(BASE_DIR + File.separator + playlistName);
+//    private void checkPlaylistInTarget(String playlistName, String targetDir, List<String> fileNames, int numberOfEntries) throws IOException {
+//        String baseFolder = targetDir != null ? targetDir : BASE_DIR;
+//        File playlistFile = new File(baseFolder + File.separator + playlistName);
+//        assertThat("playlist does not exists: " + playlistFile, playlistFile.getAbsoluteFile().exists(), equalTo(true));
+//
+//        List<String> fileContent = IOUtils.readLines(new FileInputStream(playlistFile));
+//        assertNotNull(fileContent);
+//        assertThat(fileContent.size(), equalTo(numberOfEntries));
+//
+//        for (int i = 0; i < fileContent.size(); i++) {
+//            assertThat(fileContent.get(i), containsString(getPathFor(fileNames.get(i))));
+//        }
+//    }
+
+    private void checkPlaylistContains(String playlistName, String targetDir, List<String> fileNames, int numberOfEntries) throws IOException {
+        File playlistFile = new File(targetDir + File.separator + playlistName);
         assertThat("playlist does not exists: " + playlistFile, playlistFile.getAbsoluteFile().exists(), equalTo(true));
 
         List<String> fileContent = FileUtils.readLines(playlistFile);
         assertNotNull(fileContent);
+        assertThat(fileContent.size(), equalTo(numberOfEntries));
 
         int lastIndex = 0;
-        for (int i = 0; i < fileNames.length; i++) {
-            String fileNameToCheck = fileNames[i];
+        for (int i = 0; i < fileNames.size(); i++) {
+            String fileNameToCheck = fileNames.get(i);
             int index = indexFor(fileContent, fileNameToCheck);
             assertThat(fileContent, hasItem(containsString(getPathFor(fileNameToCheck))));
             if (index > 0) {
