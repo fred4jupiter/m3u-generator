3c3a1f5b9f4aa00e8723686b277ed642d3fbadf7
fred4jupiter <hamsterhase@gmx.de>
2014-08-26 20:33:11 +0200

    cleaup

--- src/main/java/de/fred4jupiter/m3u/generator/shell/CustomBootstrap.java
+++ src/main/java/de/fred4jupiter/m3u/generator/shell/CustomBootstrap.java
@@ -3,16 +3,25 @@ package de.fred4jupiter.m3u.generator.shell;
 import org.springframework.shell.Bootstrap;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * Created by michael on 26.08.2014.
+ * Custom bootstrap class to disable the default build-in commands.
  */
 public class CustomBootstrap {
 
-    // --disableInternalCommands
+    private static final String DISABLE_INTERNAL_COMMANDS = "--disableInternalCommands";
 
     public static void main(String[] args) throws IOException {
         Bootstrap bootstrap = new Bootstrap();
-        bootstrap.main(new String[]{"--disableInternalCommands"});
+
+        List<String> argumentList = new ArrayList<String>(Arrays.asList(args));
+        if (!argumentList.contains(DISABLE_INTERNAL_COMMANDS)) {
+            argumentList.add(DISABLE_INTERNAL_COMMANDS);
+        }
+
+        bootstrap.main(argumentList.toArray(new String[0]));
     }
 }
